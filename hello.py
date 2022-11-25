68593b37d81d3700e425372537b13968e35617aa
thulanimbatha <thulanimbatha06@gmail.com>
2022-11-26 01:25:00 +0200

    added comment

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-
+# __name__ is a special attribute - used to get the name of the current file
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -9,5 +9,5 @@ def hello_world():
 def say_bye():
     return "Bye"
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
